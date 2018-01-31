a11425cffc9df03c72eb98ef3a5eb4d6b2c324e0
Kyle Hatch <ky13h4tch@gmail.com>
2018-01-31 15:33:31 -0700

    Implemented whitelist/user concern and name migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
 
-  devise_for :users
   get 'about', to: 'pages#about'
-
   get 'contact', to: 'pages#contact'
-
   get 'portfolio', to: 'pages#portfolio'
 
   root to: 'pages#home'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
